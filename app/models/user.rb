244beb997c2f5d48e49b4da15bc5b857c6346097
fantay <laurentcoll@hotmail.com>
2016-12-01 14:23:00 +0100

    ajout de la suppression si admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
-    validates :password, presence: true,length: {minimum: 6}
+    validates :password, presence: true,length: {minimum: 6}, allow_nil: true
 
     validates :password_confirmation, presence: true,length: {minimum: 6}
 
--- config/routes.rb
+++ config/routes.rb
@@ -23,4 +23,5 @@ Rails.application.routes.draw do
     get 'sessions/new'
 
 
+
 end
